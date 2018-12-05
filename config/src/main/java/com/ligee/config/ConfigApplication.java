92efb4ddcf75be7975fa3d460d0b6affd6129056
李杰 <lijie@quhao.la>
2018-12-05 15:52:30 +0800

    修改

--- config/src/main/java/com/ligee/config/ConfigApplication.java
+++ config/src/main/java/com/ligee/config/ConfigApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * Author: 李杰
+ * Author: 李杰 配置中心
  * Date: 2018/12/5 3:21 PM
  */
 @SpringBootApplication
