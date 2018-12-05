ce037ec72a38f2dae9f94f04ebcdeb6b78e8c687
李杰 <lijie@quhao.la>
2018-12-05 16:44:48 +0800

    修改配置中心配置文件

--- eurekaclient/src/main/java/com/ligee/client/controller/HelloController.java
+++ eurekaclient/src/main/java/com/ligee/client/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.ligee.client.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  * Date: 2018/12/5 9:49 AM
  */
 @RestController
+@RefreshScope
 public class HelloController {
+
+    @Value("${server.port}")
+    private int port;
 
     @GetMapping("index")
     public String index(){
-        return "Hello World!";
+        return "Hello World!,端口：" + port;
     }
-
 }
